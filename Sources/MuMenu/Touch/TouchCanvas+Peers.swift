2fdb56d96c068653a554b950e06dd10d3b3fb4a3
musesum <musesum@gmail.com>
2023-08-19 15:57:14 +0800

    refactor TextureData

---
+++ Sources/MuMenu/Touch/TouchCanvas+Peers.swift
@@ -0,0 +1,22 @@
+//  Created by warren on 12/19/22.
+
+import Foundation
+import MuPeer
+
+extension TouchCanvas: PeersControllerDelegate {
+
+    public func didChange() {
+    }
+
+    public func received(data: Data,
+                         viaStream: Bool) -> Bool {
+
+        let decoder = JSONDecoder()
+        if let item = try? decoder.decode(TouchCanvasItem.self, from: data) {
+            TouchCanvas.remoteItem(item)
+            return true
+        }
+        return false
+    }
+
+}
--- Sources/MuMenu/Touch/TouchFlo.swift
+++ Sources/MuMenu/Touch/TouchFlo.swift
@@ -5,9 +5,9 @@ import MuFlo
 import MuVisit
 import MuMetal
 
-public class TouchDraw {
+public class TouchFlo {
 
-    private var root     : Flo
+    private var root     : Flo?
     private var tilt˚    : Flo? ; var tilt    = false
     private var press˚   : Flo? ; var press   = true
     private var size˚    : Flo? ; var size    = CGFloat(1)
@@ -21,14 +21,14 @@ public class TouchDraw {
     private var dotOn˚   : Flo? // addDot(f, .began)
     private var dotOff˚  : Flo? // addDot(f, .ended)
 
-    private var texBuf: UnsafeMutablePointer<UInt32>?
-    private var texSize = CGSize.zero
-    private var viewSize = CGSize.zero
+    //???
 
-    public init(_ root: Flo, _ viewSize: CGSize) {
+    init() {
+    }
+
+    public func parseRoot(_ root: Flo) {
 
         self.root = root
-        self.viewSize = viewSize
 
         let sky    = root.bind("sky"   )
         let input  = sky .bind("input" )
@@ -50,11 +50,12 @@ public class TouchDraw {
         fill˚    = screen.bind("fill"   ){ f,_ in self.fill    = f.float   }
         dotOn˚   = draw  .bind("dot.on" ){ f,_ in self.addDot(f, .began)   }
         dotOff˚  = draw  .bind("dot.off"){ f,_ in self.addDot(f, .ended)   }
-
-        TouchCanvas.drawPoint = drawPoint
-        TouchCanvas.drawRadius = drawRadius
     }
 
+    public func setViewSize(_ size: CGSize) {
+        viewSize = size
+
+    }
     func addDot(_ flo: Flo,_ phase: UITouch.Phase) {
         if let exprs = flo.exprs,
            let x = exprs.nameAny["x"] as? FloValScalar,
@@ -77,40 +78,57 @@ public class TouchDraw {
         }
     }
 
-    /// get radius of TouchCanvasItem
-    public func drawRadius(_ item: TouchCanvasItem) -> CGFloat {
-
-        let visit = item.visit()
-        
-        // if using Apple Pencil and brush tilt is turned on
-        if item.force > 0, tilt {
-            let azi = CGPoint(x: CGFloat(-item.azimY), y: CGFloat(-item.azimX))
-            azimuth˚?.setAny(azi, .activate, visit)
-          //PrintGesture("azimuth dXY(%.2f,%.2f)", item.azimuth.dx, item.azimuth.dy)
-        }
-        
-        // if brush press is turned on
-        var radiusNow = CGFloat(1)
-        if press {
-            if force > 0 || item.azimX != 0.0 {
-                force˚?.setAny(item.force, .activate, visit) // will update local azimuth via FloGraph
-                radiusNow = size
-            } else {
-                radius˚?.setAny(item.radius, .activate, visit)
-                radiusNow = radius
-            }
-        } else {
-            radiusNow = size
-        }
-        return radiusNow
-    }
-
+  
     /**
      Either fill or draw inside texture
      - returns: true if filled, false if drawn
      */
 
-    public func drawTexture(_ texBuf: UnsafeMutablePointer<UInt32>,
+
+    // duplicate in MuMetal::MetAspect
+    public func viewPointToTexture(_ p: CGPoint, viewSize: CGSize, texSize: CGSize) -> CGPoint {
+
+        let fill = fillClip(from: texSize, to: viewSize)
+        let norm = fill.normalize()
+        let x0 = p.x / viewSize.width
+        let y0 = p.y / viewSize.height
+        let x1 = (x0 + norm.minX) * norm.width * texSize.width
+        let y1 = (y0 + norm.minY) * norm.height * texSize.height
+        return CGPoint(x: x1, y: y1)
+    }
+
+    func drawFill(_ fill: UInt32) {
+        guard let texBuf else { return }
+        
+        let w = Int(texSize.width)
+        let h = Int(texSize.height)
+        let count = w * h // count
+
+        for i in 0 ..< count {
+            texBuf[i] = fill
+        }
+    }
+    func drawData() {
+
+        let w = Int(texSize.width)
+        let h = Int(texSize.height)
+        let count = w * h // count
+
+        if let data = TextureData["TouchDraw"] {
+            data?.withUnsafeBytes { dataPtr in
+                guard let texBuf else { return }
+                let tex32Ptr = dataPtr.bindMemory(to: UInt32.self)
+                for i in 0 ..< count {
+                    texBuf[i] = tex32Ptr[i]
+                }
+            }
+            TextureData["TouchDraw"] = nil
+        }
+    }
+}
+extension TouchFlo: TouchDrawDelegate {
+
+   public func drawTexture(_ texBuf: UnsafeMutablePointer<UInt32>,
                             size: CGSize) -> Bool {
 
         self.texBuf = texBuf
@@ -171,98 +189,4 @@ public class TouchDraw {
         }
     }
 
-    // duplicate in MuMetal::MetAspect
-    public func viewPointToTexture(_ p: CGPoint, viewSize: CGSize, texSize: CGSize) -> CGPoint {
-
-        let fill = fillClip(from: texSize, to: viewSize)
-        let norm = fill.normalize()
-        let x0 = p.x / viewSize.width
-        let y0 = p.y / viewSize.height
-        let x1 = (x0 + norm.minX) * norm.width * texSize.width
-        let y1 = (y0 + norm.minY) * norm.height * texSize.height
-        return CGPoint(x: x1, y: y1)
-    }
-
-    public func drawPoint(_ point: CGPoint,
-                          _ radius: CGFloat) {
-
-        guard let texBuf else { return }
-        if point == .zero { return }
-
-        #if os(iOS)
-        let scale = UIScreen.main.scale
-        #else
-        let scale = CGFloat(3) //??? UITraitCollection().displayScale
-        #endif
-        let p = point * scale
-        let p1 = viewPointToTexture(p, viewSize: viewSize, texSize: texSize)
-
-        let r = radius * 2.0 - 1
-        let r2 = Int(r * r / 4.0)
-        let xs = Int(texSize.width)
-        let ys = Int(texSize.height)
-        let px = Int(p1.x)
-        let py = Int(p1.y)
-
-        var x0 = Int(p1.x - radius - 0.5)
-        var y0 = Int(p1.y - radius - 0.5)
-        var x1 = Int(p1.x + radius + 0.5)
-        var y1 = Int(p1.y + radius + 0.5)
-
-        if x0 < 0 { x0 += xs }
-        if y0 < 0 { y0 += ys }
-        while x1 < x0 { x1 += xs }
-        while y1 < y0 { y1 += ys }
-
-        if radius == 1 {
-            texBuf[y0 * xs + x0] = index
-            return
-        }
-
-        for y in y0 ..< y1 {
-
-            for x in x0 ..< x1  {
-
-                let xd = (x - px) * (x - px)
-                let yd = (y - py) * (y - py)
-
-                if xd + yd < r2 {
-
-                    let yy = (y + ys) % ys  // wrapped pixel y index
-                    let xx = (x + xs) % xs  // wrapped pixel x index
-                    let ii = yy * xs + xx   // final pixel x, y index into buffer
-
-                    texBuf[ii] = index     // set the buffer to value
-                }
-            }
-        }
-    }
-    func drawFill(_ fill: UInt32) {
-        guard let texBuf else { return }
-        
-        let w = Int(texSize.width)
-        let h = Int(texSize.height)
-        let count = w * h // count
-
-        for i in 0 ..< count {
-            texBuf[i] = fill
-        }
-    }
-    func drawData() {
-
-        let w = Int(texSize.width)
-        let h = Int(texSize.height)
-        let count = w * h // count
-
-        if let data = TextureData["TouchDraw"] {
-            data?.withUnsafeBytes { dataPtr in
-                guard let texBuf else { return }
-                let tex32Ptr = dataPtr.bindMemory(to: UInt32.self)
-                for i in 0 ..< count {
-                    texBuf[i] = tex32Ptr[i]
-                }
-            }
-            TextureData["TouchDraw"] = nil
-        }
-    }
 }
