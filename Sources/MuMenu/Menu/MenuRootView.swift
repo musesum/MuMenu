ca6c3fe1a5463d912015dcbcf5a2ac9cd8958b3a
musesum <musesum@gmail.com>
2025-04-11 16:39:36 +0900

    refactor

--- Sources/MuMenu/Menu/MenuRootView.swift
+++ Sources/MuMenu/Menu/MenuRootView.swift
@@ -5,40 +5,6 @@ import SwiftUI
 
 /// SwiftUI DragGesture to navigate menu
 
-public struct MenuRootView_OLD: View {
-
-    @GestureState private var touchXY: CGPoint = .zero
-    let menuVm: MenuVm
-    var cornerVm: CornerVm { menuVm.rootVm.cornerVm }
-
-
-    public init(menuVm: MenuVm) {
-        self.menuVm = menuVm
-    }
-
-    func geoFrame(_ geo: GeometryProxy) {
-        cornerVm.updateBounds(geo.frame(in: .global))
-    }
-    public var body: some View {
-        ZStack {
-            GeometryReader { geo in
-                StatusView()
-                    .frame(width: geo.size.width, height: 18, alignment: .top)
-                RootView()
-                    .environmentObject(menuVm.rootVm)
-                    .onAppear { geoFrame(geo) }
-                    .onChange(of: geo.frame(in: .global)) { geoFrame(geo) }
-                    .gesture(DragGesture(minimumDistance: 0,
-                                         coordinateSpace: .global)
-                        .updating($touchXY) { (value, touchXY, _) in touchXY = value.location })
-                    .onChange(of: touchXY) { cornerVm.updateDragXY($1) }
-                // .defersSystemGestures(on: .vertical)
-            }
-        }
-
-    }
-}
-
 public struct MenuRootView: View {
 
     @GestureState private var touchXY: CGPoint = .zero
