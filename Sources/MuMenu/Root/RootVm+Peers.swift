6f52fc57732d7b421bc30ff90ccd04210920eb06
musesum <musesum@gmail.com>
2026-02-12 12:23:48 -0800

    multiTrack multiPeer

--- Sources/MuMenu/Root/RootVm+Peers.swift
+++ Sources/MuMenu/Root/RootVm+Peers.swift
@@ -18,7 +18,9 @@ extension RootVm: @MainActor PeersDelegate {
     public func resetItem(_ item: PlayItem) {
         //...... maybe ignore?
     }
-
+    public func playItem(_ item: PlayItem, from: DataFrom) {
+        received(data: item.data, from: from)
+    }
     public func shareItem(_ item: Any) {
         guard let item = item as? MenuItem,
             item.policy.contains(.share) else { return }
